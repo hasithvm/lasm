0d74b32a267952683bc54e1730be33e7291a9f0e
Robert Nelson <robert@rnelson.ca>
2014-09-04 20:27:58 -0400

    Removed unneeded files

--- make.bat
+++
@@ -1,7 +0,0 @@
-mkdir bin
-del /Q bin\*
-call %SDK_DIR%\bin\SetEnv.cmd /x86 /Debug
-cd msvc10
-msbuild lasm.sln /nologo /verbosity:m /p:Configuration=Debug
-call %SDK_DIR%\bin\SetEnv.cmd /x86 /Release
-msbuild lasm.sln /nologo /verbosity:m /p:Configuration=Release
